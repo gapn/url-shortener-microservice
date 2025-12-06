f64ac2bfedf5610aa30e35e19db00fba01ab6ce9
gapn <82774730+gapn@users.noreply.github.com>
2025-12-07 00:08:29 +0100

    feat: Implement URL submission and basic validation

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +19,16 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res) => {
+  try {
+    let urlObject = new URL(req.body.url);
+  } catch (error) {
+    res.json({
+      error: 'invalid url'
+    })
+  }
 });
 
 app.listen(port, function() {
