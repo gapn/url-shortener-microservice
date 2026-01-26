67556584dda96f2f8415f4a015c0c0383c8e03cf
gapn <82774730+gapn@users.noreply.github.com>
2026-01-27 00:25:21 +0100

    feat: add redirection logic, implement DNS verification and duplicate check, complete FCC project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
 
 const urlData = [];
@@ -26,20 +27,56 @@ app.get('/api/hello', function(req, res) {
 app.post('/api/shorturl', (req, res) => {
   try {
     let urlObject = new URL(req.body.url);
-    const shortUrlId = urlData.length + 1;
-    const newShortUrl = {
-      original_url: req.body.url,
-      short_url: shortUrlId
+    
+    const existingEntry = urlData.find(item => item.original_url === req.body.url);
+    if (existingEntry) {
+      return res.json({
+        original_url: existingEntry.original_url,
+        short_url: existingEntry.short_url
+      });
     };
-    urlData.push(newShortUrl);
 
-    res.json(newShortUrl);
+    if (urlObject.protocol !== 'http:' && urlObject.protocol !== 'https:') {
+      return res.json({
+        error: 'invalid url'
+      })
+    };
+
+    const hostname = urlObject.hostname;
+    dns.lookup(hostname, (err) => {
+      if (err) {
+        return res.json({
+          error: 'invalid url'
+        })
+      } else {
+        const shortUrlId = urlData.length + 1;
+        const newShortUrl = {
+          original_url: req.body.url,
+          short_url: shortUrlId
+        };
+        urlData.push(newShortUrl);
+        res.json(newShortUrl);
+      }
+    })
   } catch (error) {
     res.json({
       error: 'invalid url'
     });
   };
 });
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const inputNumber = req.params.short_url
+  const foundItem = urlData.find(item => item.short_url === parseInt(inputNumber));
+
+  if (foundItem) {
+    res.redirect(foundItem.original_url);
+  } else {
+    res.json({
+      error: 'No short URL found for your input'
+    });
+  };
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
