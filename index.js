db04747e010ccfb3155c77f33ccd0b0b0a56d364
gapn <82774730+gapn@users.noreply.github.com>
2026-01-27 01:08:46 +0100

    fix: trying to fix for fcc tests

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const dns = require('dns');
 const app = express();
 
 const urlData = [];
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
+let idCounter = 1;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -19,27 +17,23 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
   try {
-    let urlObject = new URL(req.body.url);
+    const urlObject = new URL(originalUrl);
     
-    const existingEntry = urlData.find(item => item.original_url === req.body.url);
+    if (urlObject.protocol !== 'http:' && urlObject.protocol !== 'https:') {
+          return res.json({
+            error: 'invalid url'
+          })
+        };
+
+    const existingEntry = urlData.find(item => item.original_url === originalUrl);
     if (existingEntry) {
       return res.json({
         original_url: existingEntry.original_url,
         short_url: existingEntry.short_url
       });
-    };
-
-    if (urlObject.protocol !== 'http:' && urlObject.protocol !== 'https:') {
-      return res.json({
-        error: 'invalid url'
-      })
     };
 
     const hostname = urlObject.hostname;
@@ -48,15 +42,14 @@ app.post('/api/shorturl', (req, res) => {
         return res.json({
           error: 'invalid url'
         })
-      } else {
-        const shortUrlId = urlData.length + 1;
-        const newShortUrl = {
-          original_url: req.body.url,
-          short_url: shortUrlId
-        };
-        urlData.push(newShortUrl);
-        res.json(newShortUrl);
       }
+
+      const newEntry = {
+        original_url: originalUrl,
+        short_url: idCounter++
+      };
+      urlData.push(newEntry);
+      res.json(newEntry);
     })
   } catch (error) {
     res.json({
@@ -66,11 +59,14 @@ app.post('/api/shorturl', (req, res) => {
 });
 
 app.get('/api/shorturl/:short_url', (req, res) => {
-  const inputNumber = req.params.short_url
-  const foundItem = urlData.find(item => item.short_url === parseInt(inputNumber));
+  const shortUrl = parseInt(req.params.short_url);
+  const foundItem = urlData.find(item => item.short_url === shortUrl);
 
   if (foundItem) {
-    res.redirect(foundItem.original_url);
+    res
+      .status(302)
+      .set('Location', foundItem.original_url)
+      .end();
   } else {
     res.json({
       error: 'No short URL found for your input'
@@ -78,6 +74,7 @@ app.get('/api/shorturl/:short_url', (req, res) => {
   };
 })
 
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
